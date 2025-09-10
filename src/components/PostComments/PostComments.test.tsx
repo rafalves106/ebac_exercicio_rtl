07d2e4e979c13db679b1e1f73617c85b0531a561
Rafael <rafalves106@icloud.com>
2025-09-10 17:43:17 -0300

    Realizando testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { render, screen, fireEvent } from "@testing-library/react";
+import PostComments from "./index";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("PostComments Component", () => {
+  it("deve adicionar dois comentários na lista", async () => {
+    render(<PostComments />);
+
+    const inputComment = screen.getByTestId("input-comment");
+    const sendButton = screen.getByTestId("send-button");
+
+    fireEvent.change(inputComment, {
+      target: { value: "Primeiro comentário de teste." },
     });
+    fireEvent.click(sendButton);
+
+    fireEvent.change(inputComment, {
+      target: { value: "Segundo comentário de teste." },
+    });
+    fireEvent.click(sendButton);
+
+    const comments = await screen.findAllByTestId("comment-item");
+    expect(comments).toHaveLength(2);
+
+    expect(
+      screen.getByText("Primeiro comentário de teste.")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Segundo comentário de teste.")
+    ).toBeInTheDocument();
+  });
 });
